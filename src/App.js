6a84bcd9d9c589b1ac2a7318f9b1c8d5a3869f35
dvn <dvn2001@mail.ru>
2019-07-04 16:00:02 +0300

    Play

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +21,9 @@ function App() {
         </a>
       </header>
     </div>
-  );
+);
+  
+  }
 }
 
 export default App;
